3f93f6be00fa0efd351639e237514c81dc17310a
alonsomartinez <alonso.mtz99@gmail.com>
2018-10-18 09:52:49 -0500

    Actaulización para probar en windows

--- sitio-dai/Default.aspx.cs
+++ sitio-dai/Default.aspx.cs
@@ -5,11 +5,23 @@ using System.Web.UI;
 namespace sitiodai
 {
 
-    public partial class Default : System.Web.UI.Page
-    {
-        public void button1Clicked(object sender, EventArgs args)
-        {
-           
+    public partial class Default : System.Web.UI.Page{
+
+        // Variables de clase
+        
+        GestorBD.GestorBD gestorLocal; 
+        
+        
+        protected void Page_Load(object sender, EventArgs ev){
+            // IsPostBack guarda el estado de la página. Cargada por primera vez, o recargada
+            Console.WriteLine("Prueba Uno"); 
+            if (!IsPostBack){
+                gestorLocal = new GestorBD.GestorBD("SQLNCLI11", "localhost", "sa", "sqladmin", "proyecto-musica");
+                //Guardamos el gestor en Session para recuperarlo después
+                Session["Gestor"] = gestorLocal; 
+            }
         }
+
+
     }
 }
